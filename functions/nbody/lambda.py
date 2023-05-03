5558095c558c18bed9006f815411ef407ee34843
Alexander Mead <alexander@digilab.co.uk>
2023-05-03 10:48:05 +0100

    Fully working example

--- functions/nbody/lambda.py
+++ functions/nbody/lambda.py
@@ -39,20 +39,35 @@ def handler(event, context, verbose=True):
         "H_0": float(body["H_0"]),
         "w_0": -1.,
         "w_a": 0.,
+        "A_s": 2.1e-9,
         "sigma_8": float(body["sigma_8"]),
         "n_s": float(body["n_s"]),
         "m_nu": 0.,
     }
-    kmin = 1e-3
-    kmax = 1e1
-    nk = 128
-    z = 0.
-    L = 512.
-    n = 1000
+    kmin = float(body["kmin"])
+    kmax = float(body["kmax"])
+    nk = int(body["nk"])
+    z = float(body["z"])
+    L = float(body["Lbox"])
+    n = int(body["npix"])
     pad = 0.02
     seed = 123
+    vmin, vmax = 1e-3, 1e4  # Change these if log_normal_transform = False
+    # vmin, vmax = -15., 15.
+    cmap = "cubehelix"
     np.random.seed(seed)
-    data = nbody.make_image(params, (kmin, kmax), nk, z, L, n, pad_inches=pad)
+    box_h_units = True
+    log_normal_transform = True
+    plot_log_overdensity = True  # Should be False if log_normal_transform = False
+    norm_sigma8 = True
+    data = nbody.make_image(params, (kmin, kmax), nk, z, L, n, (vmin, vmax),
+                            box_h_units=box_h_units,
+                            log_normal_transform=log_normal_transform,
+                            plot_log_overdensity=plot_log_overdensity,
+                            norm_sigma8=norm_sigma8,
+                            cmap=cmap, pad_inches=pad,
+                            verbose=True,
+                            )
     data = base64.b64encode(data)  # Encode to base64 bytes
     data = data.decode()           # Convert bytes to string
 
--- functions/nbody/nbody.py
+++ functions/nbody/nbody.py
@@ -12,21 +12,18 @@ import hmcode
 
 # @njit(parallel=True)
 def make_Gaussian_random_field_2D(mean_value: float, power_spectrum: np.ndarray,
-                                  map_size: int, mesh_cells: int, *args) -> np.ndarray:
-
-    # mean_value: mean value for the field
-    # power_spectrum: P(k, *args) power spectrum for the field [(length units)^2]
-    # map_size: side length for the map [length units]
-    # mesh_cells: number of mesh cells for the field
-    # periodic: should the map be considered to be periodic or not?
-    # *args: extra arguments for power spectrum
+                                  map_size: int, mesh_cells: int) -> np.ndarray:
+    """
+    Parameters:
+        mean_value: mean value for the field
+        power_spectrum: P(k, *args) power spectrum for the field [(length units)^2]
+        map_size: side length for the map [length units]
+        mesh_cells: number of mesh cells for the field
+    """
 
     # TODO: Enforce Hermitian condition
     # TODO: Use real-to-real FFT
     # TODO: Generalise to nD
-
-    # Parameters
-    pad_fraction = 2  # Amount to increase size of array if non-periodic
 
     cfield = np.zeros((mesh_cells, mesh_cells), dtype=complex)
     mesh_size = map_size/mesh_cells
@@ -40,9 +37,10 @@ def make_Gaussian_random_field_2D(mean_value: float, power_spectrum: np.ndarray,
     # Fill the map in Fourier space
     k_grid = np.sqrt(kx**2+ky[:, None]**2)
     k_grid[0, 0] = 1e-10  # Avoid division by zero
-    sigma_grid = np.sqrt(power_spectrum(k_grid, *args))/map_size
+    sigma_grid = np.sqrt(power_spectrum(k_grid))/map_size
     x, y = np.random.normal(0., sigma_grid, size=(2, mesh_cells, mesh_cells))
     cfield = x + 1j*y
+    cfield[0, 0] = mean_value
 
     # FFT
     cfield = np.fft.ifft2(cfield)
@@ -54,14 +52,20 @@ def make_Gaussian_random_field_2D(mean_value: float, power_spectrum: np.ndarray,
     return field
 
 
-def make_image(params: dict, krange=(1e-3, 1e1), nk=128, z=0., L=512., n=512, log_normal=True, pad_inches=0.) -> bytes:
-
-    # Ranges
-    k = np.logspace(np.log10(krange[0]), np.log10(krange[-1]), nk)
-    zs = [z]  # Redshifts
+def make_image(params: dict, krange=(1e-3, 1e1), nk=128, z=0., L=512., n=512,
+               vrange=(None, None), box_h_units=True, truncate_Pk=True,
+               log_normal_transform=True, plot_log_overdensity=True,
+               norm_sigma8=True,
+               pad_inches=0., cmap='CMRmap',
+               verbose=False) -> bytes:
 
     # Constants
-    As = 2.1e-9
+    kfac = 10.  # How much bigger is CAMB kmax
+
+    # Ranges
+    kmin, kmax = krange
+    k = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+    zs = [z]  # Redshifts
 
     # Initial run of CAMB
     pars = camb.CAMBparams(WantCls=False)
@@ -71,21 +75,22 @@ def make_image(params: dict, krange=(1e-3, 1e1), nk=128, z=0., L=512., n=512, lo
                        H0=params['H_0'], mnu=params['m_nu'])
     pars.set_dark_energy(
         w=params['w_0'], wa=params['w_a'], dark_energy_model='ppf')
-    pars.InitPower.set_params(As=As, ns=params['n_s'])
-    pars.set_matter_power(redshifts=zs, kmax=10.*krange[-1])
+    pars.InitPower.set_params(As=params['A_s'], ns=params['n_s'])
+    pars.set_matter_power(redshifts=zs, kmax=kfac*kmax)
 
     # Scale 'As' to be correct for the desired 'sigma_8' value if necessary
-    results = camb.get_results(pars)
-    sigma_8_init = results.get_sigma8_0()
-    scaling = (params['sigma_8']/sigma_8_init)**2
-    As *= scaling
-    pars.InitPower.set_params(As=As, ns=params['n_s'])
+    if norm_sigma8:
+        results = camb.get_results(pars)
+        sigma_8_init = results.get_sigma8_0()
+        scaling = (params['sigma_8']/sigma_8_init)**2
+        params['A_s'] *= scaling
+        pars.InitPower.set_params(As=params['A_s'], ns=params['n_s'])
 
     # Run CAMB
     results = camb.get_results(pars)
 
     # HMcode
-    Pk = hmcode.power(k, zs, results)[0]  # Isolate the z=0
+    Pk = hmcode.power(k, zs, results)[zs.index(z)]
 
     # Plot
     # for iz, z in enumerate(zs):
@@ -95,35 +100,57 @@ def make_image(params: dict, krange=(1e-3, 1e1), nk=128, z=0., L=512., n=512, lo
     # plt.legend()
     # plt.show()
 
-    def Pk_func(x, k, Pk, kmax):
-        Pk = np.exp(np.interp(np.log(x), np.log(k),
-                    np.log(Pk*np.exp(-k/kmax))))
+    def Pk_func(k, k_array, Pk_array):
+        Pk = np.exp(np.interp(np.log(k), np.log(k_array), np.log(Pk_array)))
         return Pk
 
-    def Dk_func(x, k, Pk, kmax):
-        Dk = 4.*np.pi*(x/(2.*np.pi))**3*Pk_func(x, k, Pk, kmax)
+    def Dk_func(k, k_array, Pk_array):
+        Dk = 4.*np.pi*(k/(2.*np.pi))**3*Pk_func(k, k_array, Pk_array)
         return Dk
 
+    # TODO: Convert to 2D power spectrum
+
     # Calculate kmax_Pk from the power spectrum
-    kmax_Pk = fsolve(lambda x: Dk_func(x, k, Pk, 100.)-1., 0.1)
+    # Defined as a non-linear wavenumber where D^2(k_max)=1.
+    if truncate_Pk:
+        kmax_Pk = fsolve(lambda x: Dk_func(x, k, Pk)-1., 0.1)[0]
+        if verbose:
+            print(f"Truncation k: {kmax_Pk} h/Mpc")
+        Pk *= np.exp(-k/kmax_Pk)
+
+    # Generate Gaussian random field
+    # TODO: Check the length transformation
+    L_here = L if box_h_units else L/(params['H_0']/100.)
     delta = make_Gaussian_random_field_2D(
-        0., lambda x: Pk_func(x, k, Pk, kmax_Pk), L, n)
-    # print(delta.min(), delta.max(), delta.mean(), delta.std())
-    if log_normal:  # Log-normal transform
+        0., lambda x: Pk_func(x, k, Pk), L_here, n)
+
+    # Log-normal transform
+    if log_normal_transform:
         delta = np.exp(delta)-1.
         delta = -1.+(1.+delta)/(1.+delta.mean())
-        # print(delta.min(), delta.max(), delta.mean(), delta.std())
+    if verbose:
+        print(f"Minimum and maximum field values: {delta.min(), delta.max()}")
+        print(f"Mean and standard deviation: {delta.mean(), delta.std()}")
+        print()
 
-    eps = 1.+delta[delta > -1.].min()
-    buffer = io.BytesIO()
+    # Plot
     plt.subplots(figsize=(8, 8), dpi=224)
-    plt.imshow(np.log(1.+delta+eps))
+    vmin, vmax = vrange
+    if plot_log_overdensity:
+        eps = 1.+delta[delta > -1.].min()
+        vmin_here = np.log10(vmin) if vmin is not None else None
+        vmax_here = np.log10(vmax) if vmax is not None else None
+        plt.imshow(np.log10(1.+delta+eps), vmin=vmin_here,
+                   vmax=vmax_here, cmap=cmap)
+    else:
+        plt.imshow(1.+delta, vmin=vmin, vmax=vmax, cmap=cmap)
     plt.xticks([])
     plt.yticks([])
+    buffer = io.BytesIO()
     plt.savefig(buffer, bbox_inches='tight',
                 format='png', pad_inches=pad_inches)
     buffer = buffer.getvalue()
-    return buffer   # Return the image binary (avoids saving to disk)
+    return buffer  # Return the image binary
 
 
 if __name__ == "__main__":
@@ -144,8 +171,11 @@ if __name__ == "__main__":
     L = 512.
     n = 512
     seed = 123
+    vmin, vmax = 1e-3, 5000.
+    cmap = 'cubehelix'
 
     np.random.seed(seed)
 
-    _ = make_image(params, (kmin, kmax), nk, z, L, n)
+    _ = make_image(params, (kmin, kmax), nk, z, L, n,
+                   vrange=(vmin, vmax), cmap=cmap, verbose=True)
     plt.show()
