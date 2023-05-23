b3abb3d7da05e33a822ce53c8ab3d88d01b220b0
Alexander Mead <alexander.j.mead@googlemail.com>
2023-05-23 20:59:55 +0100

    changed campaign name

--- functions/nbody/nbody.py
+++ functions/nbody/nbody.py
@@ -186,7 +186,7 @@ def get_Pk3D_twinLab(params: dict, k: np.array, z, verbose=False):
         "m_nu": params["m_nu"],
     }
     df = pd.DataFrame(_params, index=[0])
-    campaign = "cosmology-app"
+    campaign = "universe"
     k_here = np.logspace(np.log10(1e-3), np.log10(1e1),
                          100)  # This must be the same!
     if not np.allclose(k, k_here):
