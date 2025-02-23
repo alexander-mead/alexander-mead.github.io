88fb7983fa62920d58f49840ea1fdffbb5d48fb7
Alexander Mead <alexander@digilab.co.uk>
2025-02-23 17:20:37 +0000

    Moved Mandelbrot build files to dir

---
+++ functions/mandelbrot/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+# Parse command-line arguments
+if [ "$#" -ne 1 ]; then
+    echo "Usage: build.sh <local|cloud|guided>"
+    exit 1
+fi
+
+# Build
+sam build
+
+# Deploy locally or to the cloud
+if [ "$1" = "local" ]; then
+    sam local start-api
+elif [ "$1" = "cloud" ]; then
+    sam deploy
+elif [ "$1" = "guided" ]; then
+    sam deploy --guided
+else
+    echo "Invalid build option: $1."
+    exit 1
+fi
+exit 0
